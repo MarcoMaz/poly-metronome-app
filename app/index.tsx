63bfb86380f9151033bd387c6d01e35b0b7ffe9c
Marco Mazzai <mazzai.marco@gmail.com>
2022-09-13 20:37:30 +0200

    basic first version

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,103 @@
-/* eslint-disable no-console */
-console.log('yayo');
+var audioContext: AudioContext = null;
+var isPlaying = false;          // Are we currently playing?
+var current16thNote: number;            // What note is currently last scheduled?
+var againstBeat = 3;
+var baseBeat = 4;
+var tempo = 120.0;              // Tempo (in beats per minute)
+var lookahead = 25.0;           // How frequently to call scheduling function (in milliseconds)
+var scheduleAheadTime = 0.1;    // How far ahead to schedule audio (sec). This is calculated from lookahead, and overlaps with next interval (in case the timer is late)
+var nextNoteTime = 0.0;         // when the next note is due.
+var noteLength = 0.05;          // length of "beep" (in seconds)
+var notesInQueue = [];          // the notes that have been put into the web audio, and may or may not have played yet. {note, time}
+var timerWorker: Worker = null;         // The Web Worker used to fire timer messages
 
-const myWorker = new Worker(new URL('../workers/worker.tsx', import.meta.url));
+const nextNote = () => {
+  var secondsPerBeat = 60.0 / tempo;
+  nextNoteTime += 0.25 * secondsPerBeat;
 
-myWorker.postMessage(2);
-
-let result: any;
-
-console.log('myWorker', myWorker);
-
-myWorker.onmessage = (e) => {
-  result = e.data;
-  console.log('Message receives from worker');
-};
-
-console.log('result is ', result);
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
-  })
+  current16thNote++;
+  if (current16thNote == againstBeat * baseBeat) current16thNote = 0;
 }
+
+const scheduleNote = ( beatNumber: number, time: number ) => {
+  notesInQueue.push( { note: beatNumber, time: time } );
+
+  console.log('beatNumber', beatNumber);
+
+  if (beatNumber % baseBeat !== 0 && beatNumber % againstBeat !== 0) return; // we're not playing non-8th 16th notes
+
+  // create an oscillator
+  var osc = audioContext.createOscillator();
+  osc.connect( audioContext.destination );
+  if (beatNumber % (baseBeat * againstBeat) === 0) osc.frequency.value = 880.0;
+  else if (beatNumber % againstBeat === 0) osc.frequency.value = 220.0;
+  else if (beatNumber % baseBeat === 0) osc.frequency.value = 440.0;
+
+  osc.start( time );
+  osc.stop( time + noteLength );
+}
+
+const scheduler = () => {
+  while (nextNoteTime < audioContext.currentTime + scheduleAheadTime ) {
+    scheduleNote( current16thNote, nextNoteTime );
+    nextNote();
+  }
+}
+
+const play = () => {
+  isPlaying = !isPlaying;
+
+  if (isPlaying) {
+    current16thNote = 0;
+    nextNoteTime = audioContext.currentTime;
+    timerWorker.postMessage("start");
+    playButton.innerHTML = 'stop';
+  } else {
+    timerWorker.postMessage("stop");
+    playButton.innerHTML = 'play';
+  }
+}
+
+const init = () => {
+  audioContext = new AudioContext();
+  timerWorker = new Worker(new URL('../workers/worker.tsx', import.meta.url));
+  timerWorker.onmessage = (e) => (e.data == "tick") ? scheduler() : console.log("message: " + e.data);
+  timerWorker.postMessage({"interval": lookahead});
+}
+
+const playButton = document.querySelector('.play');
+
+playButton.addEventListener('click', () => {
+  play();
+})
+
+const showTempo = document.getElementById('showTempo');
+const inputTempo = document.getElementById('showTempo');
+
+inputTempo.addEventListener('input', (event) => {
+  tempo = (event.target as HTMLInputElement).value as unknown as number;
+  console.log('tempo is ', tempo);
+  showTempo.innerText = `${tempo}`;
+})
+
+const showAgainstBeat = document.getElementById('againstBeat');
+const inputAgainstBeat = document.getElementById('againstSlider');
+
+inputAgainstBeat.addEventListener('input', (event) => {
+  againstBeat = (event.target as HTMLInputElement).value as unknown as number;
+  showAgainstBeat.innerText = `${againstBeat}`;
+  console.log('againstBeat', againstBeat);
+})
+
+const showBaseBeat = document.getElementById('baseBeat');
+const inputBaseBeat = document.getElementById('baseSlider');
+
+inputBaseBeat.addEventListener('input', (event) => {
+  baseBeat = (event.target as HTMLInputElement).value as unknown as number;
+  showBaseBeat.innerText = `${baseBeat}`;
+  console.log('baseBeat', baseBeat);
+})
+
+
+
+window.addEventListener("load", init );
--- workers/worker.tsx
+++ workers/worker.tsx
@@ -1,7 +1,24 @@
-/* eslint-disable no-console */
-onmessage = (e) => {
-  console.log('Message received from main script');
-  const workerResult = `message back ${e.data * 8}`;
-  console.log(workerResult);
-  postMessage(workerResult);
+var timerID: string | number | NodeJS.Timer = null;
+var interval = 100;
+
+self.onmessage = (e) => {
+	if (e.data=="start") {
+		console.log("//WORKER: starting");
+		timerID = setInterval(function(){ postMessage("tick")}, interval ) 
+	} else if (e.data.interval) {
+    console.log("//WORKER: setting interval");
+		interval = e.data.interval;
+		console.log("//WORKER: interval=" + interval);
+
+		if (timerID) {
+			clearInterval(timerID);
+			timerID = setInterval( function(){ postMessage("tick")}, interval )
+		}
+	} else if (e.data=="stop") {
+		console.log("//WORKER: stopping");
+		clearInterval(timerID);
+		timerID = null;
+	}
 };
+
+postMessage('hi there');
