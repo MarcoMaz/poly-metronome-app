5671100e2935b49bbb0059e81c18d55c0215f587
Marco Mazzai <mazzai.marco@gmail.com>
2022-11-30 22:39:20 +0100

    fix grid and dot shape

--- app/classes/View/GuiContainer/Shape.ts
+++ app/classes/View/GuiContainer/Shape.ts
@@ -1,126 +1,18 @@
 import Metronome from "../../Metronome";
 import { DESKTOP_VIEWPORT, MOBILE_VIEWPORT, TABLET_VIEWPORT } from "./CanvasUi";
 
-//// GENERAL
+// Colors
 const ACTIVE_BEAT_COLOR_OTHER = "lightblue";
 const ACTIVE_BEAT_COLOR_CURRENT = "blue";
 const INACTIVE_BEAT_COLOR = "lightgray";
-// const HORIZONTAL_GAP_BETWEEN_ELEMENTS = 100;
-// const VERTICAL_GAP_BETWEEN_ELEMENTS = 10;
-
-class Shape {
-  size: number;
-  index: number;
-  beatType: string;
-  currentNote: number;
-  type: string;
-
-  constructor(
-    public canvas: HTMLCanvasElement,
-    public myCanvasContext: CanvasRenderingContext2D,
-    public metronome: Metronome,
-    index: number,
-    beatType: "base" | "against",
-    type: string,
-    currentNote?: number
-  ) {
-    this.index = index;
-    this.beatType = beatType;
-    this.type = type;
-    this.currentNote = currentNote;
-  }
-  
-  public createShape(): void {
-    this.resize();
-
-    // Squares / Pipelines
-    let rectX: number;
-    let rectY: number;
-    let rectHeight: number;
-    let rectWidth: number;
-    let padding: number;
-    
-    if (this.beatType === "against") {
-      // Squares / Pipelines
-      padding = (this.canvas.width / this.metronome.againstBeat) - this.size;
-      rectY = 0;
-    } else {
-      // Squares / Pipelines
-      padding = (this.canvas.width / this.metronome.baseBeat) - this.size;
-      rectY = this.canvas.height - this.size * 2;
-    }
-    
-    switch (this.type) {
-      case "square":
-        rectX = (this.size + padding) * this.index
-        rectWidth = this.size * 2;
-        rectHeight = rectWidth;
-        this.myCanvasContext.fillRect(rectX, rectY, rectWidth, rectHeight);
-        this.myCanvasContext.fill();
-        break;
-      case "pipelines":
-        rectX = (this.size + padding) * this.index
-        rectWidth = this.size / 2;
-        rectHeight = this.size * 2;
-        this.myCanvasContext.fillRect(rectX, rectY, rectWidth, rectHeight);
-        this.myCanvasContext.fill();
-        break;
-    }
-  }
-
-  public render(): void {
-    this.myCanvasContext.fillStyle = INACTIVE_BEAT_COLOR;
-    this.createShape();
-  }
-
-  public animate(): void {
-    let metronomeType: number;
-
-    this.beatType === "against"
-      ? (metronomeType = this.metronome.baseBeat)
-      : (metronomeType = this.metronome.againstBeat);
-
-    if (this.currentNote % metronomeType === 0) {
-      this.myCanvasContext.fillStyle =
-        this.currentNote / metronomeType === this.index
-          ? ACTIVE_BEAT_COLOR_CURRENT
-          : ACTIVE_BEAT_COLOR_OTHER;
-    } else {
-      this.myCanvasContext.fillStyle = ACTIVE_BEAT_COLOR_OTHER;
-    }
-    this.createShape();
-  }
-
-  public resize(): void {
-    if (window.innerWidth >= MOBILE_VIEWPORT && window.innerWidth < TABLET_VIEWPORT) {    
-      this.size = 16;  
-    } else if (window.innerWidth >= TABLET_VIEWPORT && window.innerWidth < DESKTOP_VIEWPORT) {      
-      this.size = 20;
-    } else {     
-      this.size = 32; 
-    }
-  }
-}
-
-export default Shape;
-
-/*
-import Metronome from "../../Metronome";
-
-//// GENERAL
-const ACTIVE_BEAT_COLOR_OTHER = "lightblue";
-const ACTIVE_BEAT_COLOR_CURRENT = "blue";
-const INACTIVE_BEAT_COLOR = "lightgray";
-const HORIZONTAL_GAP_BETWEEN_ELEMENTS = 100;
-const VERTICAL_GAP_BETWEEN_ELEMENTS = 10;
-
-//// GRID
 const GRID_BORDER_COLOR = "black";
-const GRID_CELL_SIZE = 70;
-const VERTICAL_GAP_BETWEEN_GRID_CELLS = 35;
 
-//// Dot
-const DOT_RADIUS = 10;
+// Sizes
+const SIZE_MOBILE = 16;
+const SIZE_TABLET = 20;
+const SIZE_DESKTOP = 32;
+
+// Angles
 const DOT_START_ANGLE = 0;
 const DOT_END_ANGLE = 2 * Math.PI;
 
@@ -140,14 +32,34 @@ class Shape {
     type: string,
     currentNote?: number
   ) {
-    this.size = 16;
     this.index = index;
     this.beatType = beatType;
     this.type = type;
     this.currentNote = currentNote;
   }
 
+  public resize(): void {
+    if (
+      window.innerWidth >= MOBILE_VIEWPORT &&
+      window.innerWidth < TABLET_VIEWPORT
+    ) {
+      this.size = SIZE_MOBILE;
+    } else if (
+      window.innerWidth >= TABLET_VIEWPORT &&
+      window.innerWidth < DESKTOP_VIEWPORT
+    ) {
+      this.size = SIZE_TABLET;
+    } else {
+      this.size = SIZE_DESKTOP;
+    }
+  }
+
   public createShape(): void {
+    this.resize();
+
+    // General
+    let padding: number;
+
     // Squares / Pipelines
     let rectX: number;
     let rectY: number;
@@ -157,65 +69,50 @@ class Shape {
     // Grid
     let gridX: number;
     let gridY: number;
-    let gridWidth: number;
     let gridHeight: number;
+    let gridWidth: number;
 
     // Dots
     let dotX: number;
     let dotY: number;
+    let dotRadius: number;
 
     if (this.beatType === "against") {
-      // Squares / Pipelines
-      rectX = this.size + this.index * HORIZONTAL_GAP_BETWEEN_ELEMENTS;
-      rectY = VERTICAL_GAP_BETWEEN_ELEMENTS;
-
-      // Grid
-      gridX = this.size + this.index * GRID_CELL_SIZE;
-      gridY = VERTICAL_GAP_BETWEEN_GRID_CELLS;
-      gridWidth = GRID_CELL_SIZE;
-      gridHeight = this.size;
-
-      // Dots
-      dotX = rectX;
-      dotY = rectY;
+      padding = this.canvas.width / this.metronome.againstBeat - this.size;
+      rectY = 0;
+      dotY = this.size;
+      gridX = (this.canvas.width / this.metronome.againstBeat) * this.index;
+      gridY = 0;
+      gridWidth = this.canvas.width / this.metronome.againstBeat;
     } else {
-      // Squares / Pipelines
-      rectX =
-        this.size +
-        ((this.index * HORIZONTAL_GAP_BETWEEN_ELEMENTS) /
-          this.metronome.baseBeat) *
-          this.metronome.againstBeat;
-      rectY = this.size;
-
-      // Grid
-      gridX =
-        this.size +
-        ((this.index * GRID_CELL_SIZE) / this.metronome.baseBeat) *
-          this.metronome.againstBeat;
-      gridY = this.size;
-      gridWidth =
-        (GRID_CELL_SIZE / this.metronome.baseBeat) * this.metronome.againstBeat;
-      gridHeight = this.size;
-
-      // Dots
-      dotX = rectX;
-      dotY = rectY;
+      padding = this.canvas.width / this.metronome.baseBeat - this.size;
+      rectY = this.canvas.height - this.size * 2;
+      dotY = this.canvas.height - this.size;
+      gridX = (this.canvas.width / this.metronome.baseBeat) * this.index;
+      gridY = this.canvas.height / 2;
+      gridWidth = this.canvas.width / this.metronome.baseBeat;
     }
 
     switch (this.type) {
       case "square":
-        rectWidth = 21;
-        rectHeight = this.size / 2;
+        rectX = (this.size + padding) * this.index;
+        rectWidth = this.size * 2;
+        rectHeight = rectWidth;
+
         this.myCanvasContext.fillRect(rectX, rectY, rectWidth, rectHeight);
         this.myCanvasContext.fill();
         break;
       case "pipelines":
-        rectWidth = 1;
-        rectHeight = this.size / 2;
+        rectX = (this.size + padding) * this.index;
+        rectWidth = this.size / 2;
+        rectHeight = this.size * 2;
+
         this.myCanvasContext.fillRect(rectX, rectY, rectWidth, rectHeight);
         this.myCanvasContext.fill();
         break;
       case "grid":
+        gridHeight = this.canvas.height / 2;
+
         this.myCanvasContext.beginPath();
         this.myCanvasContext.rect(gridX, gridY, gridWidth, gridHeight);
         this.myCanvasContext.strokeStyle = GRID_BORDER_COLOR;
@@ -223,11 +120,14 @@ class Shape {
         this.myCanvasContext.stroke();
         break;
       case "dots":
+        dotX = (this.size + padding) * this.index + this.size;
+        dotRadius = this.size;
+
         this.myCanvasContext.beginPath();
         this.myCanvasContext.arc(
           dotX,
           dotY,
-          DOT_RADIUS,
+          dotRadius,
           DOT_START_ANGLE,
           DOT_END_ANGLE
         );
@@ -261,4 +161,3 @@ class Shape {
 }
 
 export default Shape;
-*/
