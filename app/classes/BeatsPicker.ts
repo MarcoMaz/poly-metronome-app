a59be14ec031e9bf99abbb300148c1a709a94a9f
Marco Mazzai <mazzai.marco@gmail.com>
2023-04-16 21:16:38 +0200

    fix( beatsPicker ): - remove extra empty spans - fix styling

--- app/classes/BeatsPicker.ts
+++ app/classes/BeatsPicker.ts
@@ -4,7 +4,7 @@ import Metronome from "./Metronome";
 
 const BEAT_PICKER_SELECTOR = ".beatPicker";
 const BEAT_PICKER_BEATS_SELECTOR = ".beatPicker__beats";
-const BEAT_PICKER_ITEM = ".beatPicker__item";
+const BEAT_PICKER_ITEM_SELECTOR = ".beatPicker__item";
 const BEAT_PICKER_AIM_CLASS = "beatPicker__aim";
 
 class BeatsPicker {
@@ -25,9 +25,8 @@ class BeatsPicker {
     });
   }
 
-  private createBeatPickerItemSpan(beat: number | null): string {
-    const content = beat === null ? "" : beat;
-    return `<span class="beatPicker__item">${content}</span>`;
+  private createBeatPickerItemSpan(beat: number): string {
+    return `<span class="beatPicker__item">${beat}</span>`;
   }
 
   private createElements(
@@ -36,14 +35,11 @@ class BeatsPicker {
     parentSelector: string
   ): void {
     const missingNumbers: Array<number | null> = [
-      null,
-      null,
       ...Array.from(
         { length: secondNumber - firstNumber + 1 },
         (_, i) => i + firstNumber
       ),
     ];
-    missingNumbers.push(null, null);
 
     const spans = missingNumbers.map(this.createBeatPickerItemSpan).join("");
 
@@ -56,8 +52,9 @@ class BeatsPicker {
   }
 
   private centerBeatOnLoad(num: number): number {
-    const centeredBeatCorrectIndex = num + 1;
-    const centerItemSelector = `${BEAT_PICKER_ITEM}:nth-of-type(${centeredBeatCorrectIndex})`;
+    const centerItemSelector = `${BEAT_PICKER_ITEM_SELECTOR}:nth-of-type(${
+      num - 1
+    })`;
     const centerItem = this.beatsContainer.querySelector(
       centerItemSelector
     ) as HTMLElement;
@@ -82,14 +79,15 @@ class BeatsPicker {
     const pickerBounds = this.element.getBoundingClientRect();
     const centerLineY =
       window.pageYOffset + pickerBounds.top + pickerBounds.height / 2;
-    return Array.from(this.element.querySelectorAll(BEAT_PICKER_ITEM)).find(
-      (item) => {
-        const itemBounds = item.getBoundingClientRect();
-        const itemTopY = window.pageYOffset + itemBounds.top;
-        const itemBottomY = window.pageYOffset + itemBounds.bottom;
-        return itemTopY <= centerLineY && itemBottomY >= centerLineY;
-      }
-    );
+
+    return Array.from(
+      this.element.querySelectorAll(BEAT_PICKER_ITEM_SELECTOR)
+    ).find((item) => {
+      const itemBounds = item.getBoundingClientRect();
+      const itemTopY = window.pageYOffset + itemBounds.top;
+      const itemBottomY = window.pageYOffset + itemBounds.bottom;
+      return itemTopY <= centerLineY && itemBottomY >= centerLineY;
+    });
   }
 
   private getCenterBeat(): void {
@@ -101,10 +99,9 @@ class BeatsPicker {
 export default BeatsPicker;
 
 // ADESSO
-// 5. Fix how the index is counted. We are starting at 2. --> centerBeatOnLoad ... trying maybe using padding and no extra spans?
-// 6. Redundant code (Recurring positioning definitions // elements is redundant --> getCenterBeat)
-// 7. Fix before and after
-// 8. Make aim bigger??
+// 1. Fix how the index is counted. We are starting at 2.
+// 2. Fix before and after
+// 3. Make aim bigger??
 
 // DOPO
 //
