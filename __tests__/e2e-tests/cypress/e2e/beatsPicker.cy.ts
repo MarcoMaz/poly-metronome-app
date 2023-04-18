c29043e3f67ffb33924c4dbfeaef5bff53e6b648
Marco Mazzai <mazzai.marco@gmail.com>
2023-04-18 22:23:48 +0200

    fix( beatsPicker ): clean up tests

--- __tests__/e2e-tests/cypress/e2e/beatsPicker.cy.ts
+++ __tests__/e2e-tests/cypress/e2e/beatsPicker.cy.ts
@@ -3,183 +3,138 @@ import {
   URL,
 } from "../../../../app/classes/base/constants";
 
+const AGAINST_BEAT_PICKER_ITEM_CENTER_SELECTOR =
+  ".beatPicker.beatPicker--againstBeat .beatPicker__beats .beatPicker__item.-center";
+const BASE_BEAT_PICKER_ITEM_CENTER_SELECTOR =
+  ".beatPicker.beatPicker--baseBeat .beatPicker__beats .beatPicker__item.-center";
+
+const AGAINST_BEAT_PICKER_BEATS_SELECTOR =
+  ".beatPicker.beatPicker--againstBeat .beatPicker__beats";
+const BASE_BEAT_PICKER_BEATS_SELECTOR =
+  ".beatPicker.beatPicker--baseBeat .beatPicker__beats";
+
 describe("Against Beat Picker", () => {
   describe("on app loading", () => {
+    beforeEach(() => {
+      cy.visit(URL);
+    });
+
     it("should center the number 3", () => {
-      cy.visit(URL);
-      cy.get(
-        ".beatPicker.beatPicker--againstBeat .beatPicker__beats .beatPicker__item.-center"
-      ).should("have.text", "3");
+      cy.get(AGAINST_BEAT_PICKER_ITEM_CENTER_SELECTOR).should("have.text", "3");
     });
-
     it("should have no visible picker", () => {
-      cy.visit(URL);
-      cy.get(
-        ".beatPicker.beatPicker--againstBeat .beatPicker__beats"
-      ).should("not.have.class", "-open");
-    });
-
-    it(`should center "2" on scroll up`, () => {
-      cy.visit(URL);
-      cy.get(".beatPicker.beatPicker--againstBeat .beatPicker__beats").scrollTo(
-        "top"
+      cy.get(AGAINST_BEAT_PICKER_BEATS_SELECTOR).should(
+        "not.have.class",
+        "-open"
       );
-      cy.wait(2000)
-      cy.get(
-        ".beatPicker.beatPicker--againstBeat .beatPicker__beats .beatPicker__item.-center"
-      ).should("have.text", "2");
+    });
+    it(`should center "2" on scroll up`, () => {
+      cy.get(AGAINST_BEAT_PICKER_BEATS_SELECTOR).scrollTo("top");
+      cy.wait(2000);
+      cy.get(AGAINST_BEAT_PICKER_ITEM_CENTER_SELECTOR).should("have.text", "2");
     });
     it(`should center the number "9" on scroll down`, () => {
-      cy.visit(URL);
-      cy.get(".beatPicker.beatPicker--againstBeat .beatPicker__beats").scrollTo(
-        "bottom"
-      );
-      cy.wait(2000)
-      cy.get(
-        ".beatPicker.beatPicker--againstBeat .beatPicker__beats .beatPicker__item.-center"
-      ).should("have.text", "9");
+      cy.get(AGAINST_BEAT_PICKER_BEATS_SELECTOR).scrollTo("bottom");
+      cy.wait(2000);
+      cy.get(AGAINST_BEAT_PICKER_ITEM_CENTER_SELECTOR).should("have.text", "9");
     });
   });
 
   describe("on app playing", () => {
-    it(`should center the number "2" on scroll up`, () => {
+    beforeEach(() => {
       cy.visit(URL);
       cy.get(PLAY_BUTTON_SELECTOR).click();
-      cy.get(".beatPicker.beatPicker--againstBeat .beatPicker__beats").scrollTo(
-        "top"
-      );
-      cy.wait(2000)
-      cy.get(
-        ".beatPicker.beatPicker--againstBeat .beatPicker__beats .beatPicker__item.-center"
-      ).should("have.text", "2");
+    });
+
+    it(`should center the number "2" on scroll up`, () => {
+      cy.get(AGAINST_BEAT_PICKER_BEATS_SELECTOR).scrollTo("top");
+      cy.wait(2000);
+      cy.get(AGAINST_BEAT_PICKER_ITEM_CENTER_SELECTOR).should("have.text", "2");
     });
     it(`should center the number "9" on scroll down`, () => {
-      cy.visit(URL);
-      cy.get(PLAY_BUTTON_SELECTOR).click();
-      cy.get(".beatPicker.beatPicker--againstBeat .beatPicker__beats").scrollTo(
-        "bottom"
-      );
-      cy.wait(2000)
-      cy.get(
-        ".beatPicker.beatPicker--againstBeat .beatPicker__beats .beatPicker__item.-center"
-      ).should("have.text", "9");
+      cy.get(AGAINST_BEAT_PICKER_BEATS_SELECTOR).scrollTo("bottom");
+      cy.wait(2000);
+      cy.get(AGAINST_BEAT_PICKER_ITEM_CENTER_SELECTOR).should("have.text", "9");
     });
   });
 
   describe("on app playing and stopping", () => {
-    it(`should center the number "2" on scroll up`, () => {
+    beforeEach(() => {
       cy.visit(URL);
-      cy.get(PLAY_BUTTON_SELECTOR).click();
-      cy.get(PLAY_BUTTON_SELECTOR).click();
-      cy.get(".beatPicker.beatPicker--againstBeat .beatPicker__beats").scrollTo(
-        "top"
-      );
-      cy.wait(2000)
-      cy.get(
-        ".beatPicker.beatPicker--againstBeat .beatPicker__beats .beatPicker__item.-center"
-      ).should("have.text", "2");
+      cy.get(PLAY_BUTTON_SELECTOR).click().click();
+    });
+
+    it(`should center the number "2" on scroll up`, () => {
+      cy.get(AGAINST_BEAT_PICKER_BEATS_SELECTOR).scrollTo("top");
+      cy.wait(2000);
+      cy.get(AGAINST_BEAT_PICKER_ITEM_CENTER_SELECTOR).should("have.text", "2");
     });
     it(`should center the number "9" on scroll down`, () => {
-      cy.visit(URL);
-      cy.get(PLAY_BUTTON_SELECTOR).click();
-      cy.get(PLAY_BUTTON_SELECTOR).click();
-      cy.get(".beatPicker.beatPicker--againstBeat .beatPicker__beats").scrollTo(
-        "bottom"
-      );
-      cy.wait(2000)
-      cy.get(
-        ".beatPicker.beatPicker--againstBeat .beatPicker__beats .beatPicker__item.-center"
-      ).should("have.text", "9");
+      cy.get(AGAINST_BEAT_PICKER_BEATS_SELECTOR).scrollTo("bottom");
+      cy.wait(2000);
+      cy.get(AGAINST_BEAT_PICKER_ITEM_CENTER_SELECTOR).should("have.text", "9");
     });
   });
 });
 
 describe("Base Beat Picker", () => {
   describe("on app loading", () => {
+    beforeEach(() => {
+      cy.visit(URL);
+      cy.get(PLAY_BUTTON_SELECTOR);
+    });
+
     it("should center the number 4", () => {
-      cy.visit(URL);
-      cy.get(
-        ".beatPicker.beatPicker--baseBeat .beatPicker__beats .beatPicker__item.-center"
-      ).should("have.text", "4");
+      cy.get(BASE_BEAT_PICKER_ITEM_CENTER_SELECTOR).should("have.text", "4");
     });
-
     it("should have a visible picker", () => {
-      cy.visit(URL);
-      cy.get(
-        ".beatPicker.beatPicker--baseBeat .beatPicker__beats"
-      ).should("have.class", "-open");
+      cy.get(BASE_BEAT_PICKER_BEATS_SELECTOR).should("have.class", "-open");
     });
-
     it(`should center the number "2" on scroll up`, () => {
-      cy.visit(URL);
-      cy.get(".beatPicker.beatPicker--baseBeat .beatPicker__beats").scrollTo(
-        "top"
-      );
-      cy.wait(2000)
-      cy.get(
-        ".beatPicker.beatPicker--baseBeat .beatPicker__beats .beatPicker__item.-center"
-      ).should("have.text", "2");
+      cy.get(BASE_BEAT_PICKER_BEATS_SELECTOR).scrollTo("top");
+      cy.wait(2000);
+      cy.get(BASE_BEAT_PICKER_ITEM_CENTER_SELECTOR).should("have.text", "2");
     });
     it(`should center the number "9" on scroll down`, () => {
-      cy.visit(URL);
-      cy.get(".beatPicker.beatPicker--baseBeat .beatPicker__beats").scrollTo(
-        "bottom"
-      );
-      cy.wait(2000)
-      cy.get(
-        ".beatPicker.beatPicker--baseBeat .beatPicker__beats .beatPicker__item.-center"
-      ).should("have.text", "9");
+      cy.get(BASE_BEAT_PICKER_BEATS_SELECTOR).scrollTo("bottom");
+      cy.wait(2000);
+      cy.get(BASE_BEAT_PICKER_ITEM_CENTER_SELECTOR).should("have.text", "9");
     });
   });
 
   describe("on app playing", () => {
-    it(`should center the number "2" on scroll up`, () => {
+    beforeEach(() => {
       cy.visit(URL);
       cy.get(PLAY_BUTTON_SELECTOR).click();
-      cy.get(".beatPicker.beatPicker--baseBeat .beatPicker__beats").scrollTo(
-        "top"
-      );
-      cy.wait(2000)
-      cy.get(
-        ".beatPicker.beatPicker--baseBeat .beatPicker__beats .beatPicker__item.-center"
-      ).should("have.text", "2");
+    });
+
+    it(`should center the number "2" on scroll up`, () => {
+      cy.get(BASE_BEAT_PICKER_BEATS_SELECTOR).scrollTo("top");
+      cy.wait(2000);
+      cy.get(BASE_BEAT_PICKER_ITEM_CENTER_SELECTOR).should("have.text", "2");
     });
     it(`should center the number "9" on scroll down`, () => {
-      cy.visit(URL);
-      cy.get(PLAY_BUTTON_SELECTOR).click();
-      cy.get(".beatPicker.beatPicker--baseBeat .beatPicker__beats").scrollTo(
-        "bottom"
-      );
-      cy.wait(2000)
-      cy.get(
-        ".beatPicker.beatPicker--baseBeat .beatPicker__beats .beatPicker__item.-center"
-      ).should("have.text", "9");
+      cy.get(BASE_BEAT_PICKER_BEATS_SELECTOR).scrollTo("bottom");
+      cy.wait(2000);
+      cy.get(BASE_BEAT_PICKER_ITEM_CENTER_SELECTOR).should("have.text", "9");
     });
   });
+
   describe("on app playing and stopping", () => {
-    it(`should center the number "2" on scroll up`, () => {
+    beforeEach(() => {
       cy.visit(URL);
-      cy.get(PLAY_BUTTON_SELECTOR).click();
-      cy.get(PLAY_BUTTON_SELECTOR).click();
-      cy.get(".beatPicker.beatPicker--baseBeat .beatPicker__beats").scrollTo(
-        "top"
-      );
-      cy.wait(2000)
-      cy.get(
-        ".beatPicker.beatPicker--baseBeat .beatPicker__beats .beatPicker__item.-center"
-      ).should("have.text", "2");
+      cy.get(PLAY_BUTTON_SELECTOR).click().click();
+    });
+
+    it(`should center the number "2" on scroll up`, () => {
+      cy.get(BASE_BEAT_PICKER_BEATS_SELECTOR).scrollTo("top");
+      cy.wait(2000);
+      cy.get(BASE_BEAT_PICKER_ITEM_CENTER_SELECTOR).should("have.text", "2");
     });
     it(`should center the number "9" on scroll down`, () => {
-      cy.visit(URL);
-      cy.get(PLAY_BUTTON_SELECTOR).click();
-      cy.get(PLAY_BUTTON_SELECTOR).click();
-      cy.get(".beatPicker.beatPicker--baseBeat .beatPicker__beats").scrollTo(
-        "bottom"
-      );
-      cy.wait(2000)
-      cy.get(
-        ".beatPicker.beatPicker--baseBeat .beatPicker__beats .beatPicker__item.-center"
-      ).should("have.text", "9");
+      cy.get(BASE_BEAT_PICKER_BEATS_SELECTOR).scrollTo("bottom");
+      cy.wait(2000);
+      cy.get(BASE_BEAT_PICKER_ITEM_CENTER_SELECTOR).should("have.text", "9");
     });
   });
 });
