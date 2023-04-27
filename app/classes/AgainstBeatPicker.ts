bf6f76166ecbd74941bbfe5db027d2268f985851
Marco Mazzai <mazzai.marco@gmail.com>
2023-04-27 23:40:44 +0200

    fix( beatsPicker ): clean up constants

--- app/classes/AgainstBeatPicker.ts
+++ app/classes/AgainstBeatPicker.ts
@@ -3,12 +3,12 @@ import Metronome from "./Metronome";
 import Modal from "./Modal";
 
 const BEATS_PICKER_OPEN_CLASS = "-open";
-
-const BASE_BEAT_PICKER_BEATS = document.querySelector(
-  ".beatPicker--baseBeat > .beatPicker__beats"
-);
+const BASE_BEAT_PICKER_BEATS_SELECTOR =
+  ".beatPicker--baseBeat > .beatPicker__beats";
 
 class AgainstBeatPicker extends BeatsPicker {
+  public otherPickerBeats: HTMLElement;
+
   constructor(
     el: string,
     min: number,
@@ -19,24 +19,28 @@ class AgainstBeatPicker extends BeatsPicker {
   ) {
     super(el, min, max, centerNumber, metronome, modal);
 
+    this.otherPickerBeats = document.querySelector(
+      BASE_BEAT_PICKER_BEATS_SELECTOR
+    );
+
     this.picker.addEventListener("wheel", this.handleWheel.bind(this));
     this.picker.addEventListener("click", this.handleClick.bind(this));
   }
 
   handleWheel(): void {
     this.centerNumber = this.metronome.againstBeat;
-    this.pickerBeats.classList.add(BEATS_PICKER_OPEN_CLASS);
-    BASE_BEAT_PICKER_BEATS.classList.remove(BEATS_PICKER_OPEN_CLASS);
+    this.handleClasses();
   }
 
   handleClick(): void {
-    if (!this.pickerBeats.classList.contains(BEATS_PICKER_OPEN_CLASS)) {
-      this.pickerBeats.classList.add(BEATS_PICKER_OPEN_CLASS);
-      BASE_BEAT_PICKER_BEATS.classList.remove(BEATS_PICKER_OPEN_CLASS);
-    }
+    if (!this.pickerBeats.classList.contains(BEATS_PICKER_OPEN_CLASS))
+      this.handleClasses();
+  }
+
+  handleClasses(): void {
+    this.pickerBeats.classList.add(BEATS_PICKER_OPEN_CLASS);
+    this.otherPickerBeats.classList.remove(BEATS_PICKER_OPEN_CLASS);
   }
 }
 
 export default AgainstBeatPicker;
-
-/* handleWheel and handlClick has things in common */
