5b1225d09b2831286f9fa3cd04369228d77cb5de
Marco Mazzai <mazzai.marco@gmail.com>
2023-04-28 22:19:33 +0200

    fix( beatsPicker ): - add description for BaseBeatPicker - add description for AgainstBeatPicker

--- app/classes/AgainstBeatPicker.ts
+++ app/classes/AgainstBeatPicker.ts
@@ -7,11 +7,20 @@ import {
   BEATS_PICKER_OPEN_CLASS,
 } from "./base/constants";
 
+/**
+ * This sub class of BeatsPicker represents the UI controlling the AgainstBeatPicker.
+ *
+ * @name AgainstBeatPicker
+ *
+ * @param {HTMLElement} otherPickerBeats - The pickerBeats of baseBeatPicker.
+ *
+ */
+
 class AgainstBeatPicker extends BeatsPicker {
   public otherPickerBeats: HTMLElement;
 
   constructor(
-    el: string, 
+    el: string,
     min: number,
     max: number,
     centerNumber: number,
@@ -24,20 +33,39 @@ class AgainstBeatPicker extends BeatsPicker {
       BASE_BEAT_PICKER_BEATS_SELECTOR
     );
 
+    // Register events
     this.picker.addEventListener("wheel", this.handleWheel.bind(this));
     this.picker.addEventListener("click", this.handleClick.bind(this));
   }
 
+  /**
+   * @name handleWheel
+   * @description
+   * Set the center number to the againstBeat and updates classes.
+   *
+   */
   handleWheel(): void {
     this.centerNumber = this.metronome.againstBeat;
     this.handleClasses();
   }
 
+  /**
+   * @name handleClick
+   * @description
+   * Toggle the picker class if it's not already focused.
+   *
+   */
   handleClick(): void {
     if (!this.pickerBeats.classList.contains(BEATS_PICKER_OPEN_CLASS))
       this.handleClasses();
   }
 
+  /**
+   * @name handleClasses
+   * @description
+   * Add the "-open" class to the current picker and removes it from the other one.
+   *
+   */
   handleClasses(): void {
     this.pickerBeats.classList.add(BEATS_PICKER_OPEN_CLASS);
     this.otherPickerBeats.classList.remove(BEATS_PICKER_OPEN_CLASS);
--- app/classes/BaseBeatPicker.ts
+++ app/classes/BaseBeatPicker.ts
@@ -7,9 +7,21 @@ import {
   BEATS_PICKER_OPEN_CLASS,
 } from "./base/constants";
 
+/**
+ * This sub class of BeatsPicker represents the UI controlling the BaseBeatPicker.
+ *
+ * @name BaseBeatPicker
+ *
+ * @param {HTMLElement} otherPickerBeats - The pickerBeats of againstBeatPicker.
+ *
+ */
+
 class BaseBeatPicker extends BeatsPicker {
   public otherPickerBeats: HTMLElement;
 
+  /**
+   * Define DOM Elements and Variables.
+   */
   constructor(
     el: string,
     min: number,
@@ -23,20 +35,40 @@ class BaseBeatPicker extends BeatsPicker {
     this.otherPickerBeats = document.querySelector(
       AGAINST_BEAT_PICKER_BEATS_SELECTOR
     );
+
+    // Register events
     this.picker.addEventListener("wheel", this.handleWheel.bind(this));
     this.picker.addEventListener("click", this.handleClick.bind(this));
   }
 
+  /**
+   * @name handleWheel
+   * @description
+   * Set the center number to the baseBeat and updates classes.
+   *
+   */
   handleWheel(): void {
     this.centerNumber = this.metronome.baseBeat;
     this.handleClasses();
   }
 
+  /**
+   * @name handleClick
+   * @description
+   * Toggle the picker class if it's not already focused.
+   *
+   */
   handleClick(): void {
     if (!this.pickerBeats.classList.contains(BEATS_PICKER_OPEN_CLASS))
       this.handleClasses();
   }
 
+  /**
+   * @name handleClasses
+   * @description
+   * Add the "-open" class to the current picker and removes it from the other one.
+   *
+   */
   handleClasses(): void {
     this.pickerBeats.classList.add(BEATS_PICKER_OPEN_CLASS);
     this.otherPickerBeats.classList.remove(BEATS_PICKER_OPEN_CLASS);
