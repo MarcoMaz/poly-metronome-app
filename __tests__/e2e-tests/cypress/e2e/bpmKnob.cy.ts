aabd56379b56eae86fd2b8214b414a98ecd4e68d
Marco Mazzai <mazzai.marco@gmail.com>
2023-05-11 00:09:31 +0200

    feat( bpmKnob ): add e2e tests

---
+++ __tests__/e2e-tests/cypress/e2e/bpmKnob.cy.ts
@@ -0,0 +1,137 @@
+import {
+  PLAY_BUTTON_SELECTOR,
+  BPM_KNOB_RANGE_SELECTOR,
+  URL,
+} from "../../../../app/classes/base/constants";
+
+describe("BpmKnob", () => {
+  describe("on app loading", () => {
+    beforeEach(() => {
+      cy.visit(URL);
+      cy.get(BPM_KNOB_RANGE_SELECTOR).then(($input) => {
+        $input.css("width", "100px"); // Set the width to a non-zero value
+        $input.css("height", "20px"); // Set the height to a non-zero value
+        $input.css("opacity", "1");
+      });
+    });
+
+    it("should set value to 30 after inputting a number < 30", () => {
+      cy.get(BPM_KNOB_RANGE_SELECTOR)
+        .invoke("val", 29)
+        .trigger("change")
+        .should("have.value", "30");
+    });
+    it("should set value to 30 after inputting a number = 30", () => {
+      cy.get(BPM_KNOB_RANGE_SELECTOR)
+        .invoke("val", 30)
+        .trigger("change")
+        .should("have.value", "30");
+    });
+    it("should set value to the number entered after inputting a number > 30 and < 300", () => {
+      cy.get(BPM_KNOB_RANGE_SELECTOR)
+        .invoke("val", 180)
+        .trigger("change")
+        .should("have.value", "180");
+    });
+    it("should set value to 300 after inputting a number = 300", () => {
+      cy.get(BPM_KNOB_RANGE_SELECTOR)
+        .invoke("val", 300)
+        .trigger("change")
+        .should("have.value", "300");
+    });
+    it("should set value to 300 after inputting a number > 300", () => {
+      cy.get(BPM_KNOB_RANGE_SELECTOR)
+        .invoke("val", 350)
+        .trigger("change")
+        .should("have.value", "300");
+    });
+  });
+  describe("on app playing", () => {
+    beforeEach(() => {
+      cy.visit(URL);
+      cy.get(PLAY_BUTTON_SELECTOR).click();
+      cy.get(BPM_KNOB_RANGE_SELECTOR).then(($input) => {
+        $input.css("width", "100px"); // Set the width to a non-zero value
+        $input.css("height", "20px"); // Set the height to a non-zero value
+        $input.css("opacity", "1");
+      });
+    });
+    it("should set value to 30 after inputting a number < 30", () => {
+      cy.get(BPM_KNOB_RANGE_SELECTOR)
+        .invoke("val", 29)
+        .trigger("change")
+        .should("have.value", "30");
+    });
+    it("should set value to 30 after inputting a number = 30", () => {
+      cy.get(BPM_KNOB_RANGE_SELECTOR)
+        .invoke("val", 30)
+        .trigger("change")
+        .should("have.value", "30");
+    });
+    it("should set value to the number entered after inputting a number > 30 and < 300", () => {
+      cy.get(BPM_KNOB_RANGE_SELECTOR)
+        .invoke("val", 180)
+        .trigger("change")
+        .should("have.value", "180");
+    });
+    it("should set value to 300 after inputting a number = 300", () => {
+      cy.get(BPM_KNOB_RANGE_SELECTOR)
+        .invoke("val", 300)
+        .trigger("change")
+        .should("have.value", "300");
+    });
+    it("should set value to 300 after inputting a number > 300", () => {
+      cy.get(BPM_KNOB_RANGE_SELECTOR)
+        .invoke("val", 350)
+        .trigger("change")
+        .should("have.value", "300");
+    });
+  });
+  describe("on app playing and stopping", () => {
+    beforeEach(() => {
+      cy.visit(URL);
+      cy.get(PLAY_BUTTON_SELECTOR).click();
+      cy.get(BPM_KNOB_RANGE_SELECTOR).then(($input) => {
+        $input.css("width", "100px"); // Set the width to a non-zero value
+        $input.css("height", "20px"); // Set the height to a non-zero value
+        $input.css("opacity", "1");
+      });
+      cy.get(PLAY_BUTTON_SELECTOR).click();
+    });
+    it("should set value to 30 after inputting a number < 30", () => {
+      cy.get(BPM_KNOB_RANGE_SELECTOR)
+        .invoke("val", 29)
+        .trigger("change")
+        .should("have.value", "30");
+    });
+    it("should set value to 30 after inputting a number = 30", () => {
+      cy.get(BPM_KNOB_RANGE_SELECTOR)
+        .invoke("val", 30)
+        .trigger("change")
+        .should("have.value", "30");
+    });
+    it("should set value to the number entered after inputting a number > 30 and < 300", () => {
+      cy.get(BPM_KNOB_RANGE_SELECTOR)
+        .invoke("val", 180)
+        .trigger("change")
+        .should("have.value", "180");
+    });
+    it("should set value to 300 after inputting a number = 300", () => {
+      cy.get(BPM_KNOB_RANGE_SELECTOR)
+        .invoke("val", 300)
+        .trigger("change")
+        .should("have.value", "300");
+    });
+    it("should set value to 300 after inputting a number > 300", () => {
+      cy.get(BPM_KNOB_RANGE_SELECTOR)
+        .invoke("val", 350)
+        .trigger("change")
+        .should("have.value", "300");
+      cy.get(BPM_KNOB_RANGE_SELECTOR).then(($input) => {
+        $input.css("width", "");
+        $input.css("height", "");
+        $input.css("opacity", "");
+      });
+    });
+  });
+});
