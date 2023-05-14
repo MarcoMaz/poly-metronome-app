f556c7cddf1694d68dfb27995414400ec4cff8ec
Marco Mazzai <mazzai.marco@gmail.com>
2023-05-14 21:50:25 +0200

    - fix constants

--- app/classes/base/constants.ts
+++ app/classes/base/constants.ts
@@ -1,70 +1,6 @@
-// CanvasUi
-export const CANVAS_SELECTOR = ".Canvas";
-export const CANVAS_PIXEL_WIDTH_PHONE = 350;
-export const CANVAS_PIXEL_HEIGHT_PHONE = 130;
-export const CANVAS_PIXEL_WIDTH_TABLET = 450;
-export const CANVAS_PIXEL_HEIGHT_TABLET = 100;
-export const CANVAS_PIXEL_WIDTH_DESKTOP = 600;
-export const CANVAS_PIXEL_HEIGHT_DESKTOP = 150;
-
-// Viewports
-export const MOBILE_VIEWPORT = 0;
-export const TABLET_VIEWPORT = 700;
-export const DESKTOP_VIEWPORT = 1000;
-
-// Shape
-export const SHAPE_ACTIVE_BEAT_COLOR_OTHER = "#d1d1d1";
-export const SHAPE_ACTIVE_BEAT_COLOR_CURRENT = "#151515";
-export const SHAPE_INACTIVE_BEAT_COLOR = "#EEEEEE";
-export const SHAPE_GRID_BORDER_COLOR = "#151515";
-export const SHAPE_SIZE_MOBILE = 25;
-export const SHAPE_SIZE_TABLET = 20;
-export const SHAPE_SIZE_DESKTOP = 32;
-export const SHAPE_DOT_START_ANGLE = 0;
-export const SHAPE_DOT_END_ANGLE = 2 * Math.PI;
-
-// Engine
-export const ENGINE_SMALL_DELAY = 0.25;
-export const ENGINE_FREQUENCIES_BEAT_ONE = 880.0;
-export const ENGINE_FREQUENCIES_BASE_BEAT = 440.0;
-export const ENGINE_FREQUENCIES_AGAINST_BEAT = 220.0;
-
-// App
-export const APP_SOUND_DELAY = 0.01;
-export const URL: string = "http://192.168.0.56:3000/";
-
-// WORKER
-export const WORKER_START_MESSAGE = "start";
-export const WORKER_STOP_MESSAGE = "stop";
-
-// Restyling
-//
-// Tap
-export const TAP_CHIP_SELECTOR = ".Tap";
-
-// Modal
-export const MODAL_SELECTOR = ".Modal";
-export const MODAL_OVERLAY_SELECTOR = ".Modal__overlay";
-export const MODAL_CONTENT_SELECTOR = ".Modal__content";
-export const MODAL_TITLE_SELECTOR = ".Modal__title";
-export const MODAL_SUPPORTING_TEXT_SELECTOR = ".Modal__supporting-text";
-export const MODAL_MESSAGES_SELECTOR = ".Modal__supporting-text > p";
-export const MODAL_FIRST_MESSAGE_SELECTOR =
-  ".Modal__supporting-text p:nth-child(1)";
-export const MODAL_SECOND_MESSAGE_SELECTOR =
-  ".Modal__supporting-text p:nth-child(2)";
-export const MODAL_BUTTON_SELECTOR = ".Modal__button";
-
-// ControllerPanel
-export const CONTROLLER_PANEL_SELECTOR = ".ControllerPanel";
-export const CONTROLLER_PANEL_BUTTONS_SELECTOR = ".ControllerPanel__buttons";
-export const CONTROLLER_PANEL_BUTTON_SELECTOR = ".ControllerPanel__button";
-export const CONTROLLER_PANEL_BUTTON_BPM_SELECTOR = ".ControllerPanel__button--bpm";
-export const CONTROLLER_PANEL_BUTTON_BEATS_SELECTOR = ".ControllerPanel__button--beats";
-export const CONTROLLER_PANEL_PANE_SELECTOR = ".ControllerPanel__pane";
-export const CONTROLLER_PANEL_ACTIVE_SELECTOR = ".-active";
-export const CONTROLLER_PANEL_ACTIVE_CLASS = "-active";
-export const CONTROLLER_PANEL_SHOW_CLASS = "-show";
+///////////////////
+// Classes
+///////////////////
 
 // BeatPicker
 export const BEAT_MIN = 2;
@@ -89,7 +25,7 @@ export const BEAT_PICKER_AIM_CLASS = "BeatPicker__aim";
 // BpmKnob
 export const BPM_MIN = 30;
 export const BPM_MAX = 300;
-export const BPM_KNOB_CSS_ORIGIN_FIX = 90; 
+export const BPM_KNOB_CSS_ORIGIN_FIX = 90;
 export const BPM_KNOB_CONTAINER_SELECTOR = ".BpmKnob-container";
 export const BPM_KNOB_SELECTOR = ".BpmKnob";
 export const BPM_KNOB_INNER_TRACK_SELECTOR = ".BpmKnob__track";
@@ -99,6 +35,15 @@ export const BPM_KNOB_LABEL_SELECTOR = ".BpmKnob__label";
 export const BPM_KNOB_TEXT_SELECTOR = ".BpmKnob__text";
 export const BPM_KNOB_INACTIVE_COLOR = "#d1d1d1";
 export const BPM_KNOB_ACTIVE_COLOR = "#151515";
+
+// Canvas
+export const CANVAS_SELECTOR = ".Canvas";
+export const CANVAS_PIXEL_WIDTH_PHONE = 350;
+export const CANVAS_PIXEL_HEIGHT_PHONE = 130;
+export const CANVAS_PIXEL_WIDTH_TABLET = 450;
+export const CANVAS_PIXEL_HEIGHT_TABLET = 100;
+export const CANVAS_PIXEL_WIDTH_DESKTOP = 600;
+export const CANVAS_PIXEL_HEIGHT_DESKTOP = 150;
 
 // CanvasPicker
 export const CANVAS_PICKER_SELECTOR = ".CanvasPicker";
@@ -110,13 +55,37 @@ export const CANVAS_SELECTION_LINE = '[data-canvas-picker-button="line"]';
 export const CANVAS_SELECTION_GRID = '[data-canvas-picker-button="grid"]';
 export const CANVAS_SELECTION_DOT = '[data-canvas-picker-button="dot"]';
 
-// SwitchBeats
-export const SWITCH_BEATS_CHIP_SELECTOR = ".SwitchBeats";
+// ControllerPanel
+export const CONTROLLER_PANEL_SELECTOR = ".ControllerPanel";
+export const CONTROLLER_PANEL_BUTTONS_SELECTOR = ".ControllerPanel__buttons";
+export const CONTROLLER_PANEL_BUTTON_SELECTOR = ".ControllerPanel__button";
+export const CONTROLLER_PANEL_BUTTON_BPM_SELECTOR =
+  ".ControllerPanel__button--bpm";
+export const CONTROLLER_PANEL_BUTTON_BEATS_SELECTOR =
+  ".ControllerPanel__button--beats";
+export const CONTROLLER_PANEL_PANE_SELECTOR = ".ControllerPanel__pane";
+export const CONTROLLER_PANEL_ACTIVE_SELECTOR = ".-active";
+export const CONTROLLER_PANEL_ACTIVE_CLASS = "-active";
+export const CONTROLLER_PANEL_SHOW_CLASS = "-show";
 
-// PlayButton
-export const PLAY_BUTTON_SELECTOR = ".Play";
-export const PLAY_ICON_PLAY_SELECTOR = ".Play__icon-play";
-export const PLAY_ICON_STOP_SELECTOR = ".Play__icon-stop";
+// Engine
+export const ENGINE_SMALL_DELAY = 0.25;
+export const ENGINE_FREQUENCIES_BEAT_ONE = 880.0;
+export const ENGINE_FREQUENCIES_BASE_BEAT = 440.0;
+export const ENGINE_FREQUENCIES_AGAINST_BEAT = 220.0;
+
+// Modal
+export const MODAL_SELECTOR = ".Modal";
+export const MODAL_OVERLAY_SELECTOR = ".Modal__overlay";
+export const MODAL_CONTENT_SELECTOR = ".Modal__content";
+export const MODAL_TITLE_SELECTOR = ".Modal__title";
+export const MODAL_SUPPORTING_TEXT_SELECTOR = ".Modal__supporting-text";
+export const MODAL_MESSAGES_SELECTOR = ".Modal__supporting-text > p";
+export const MODAL_FIRST_MESSAGE_SELECTOR =
+  ".Modal__supporting-text p:nth-child(1)";
+export const MODAL_SECOND_MESSAGE_SELECTOR =
+  ".Modal__supporting-text p:nth-child(2)";
+export const MODAL_BUTTON_SELECTOR = ".Modal__button";
 
 // MuteButton
 export const MUTE_BUTTON_SELECTOR = ".Mute";
@@ -124,13 +93,52 @@ export const MUTE_BUTTON_SOUND_ON_LABEL = "SOUND: ON";
 export const MUTE_BUTTON_SOUND_OFF_LABEL = "SOUND: OFF";
 export const MUTE_BUTTON_SOUND_RESET_LABEL = "SOUND:";
 
+// PlayButton
+export const PLAY_BUTTON_SELECTOR = ".Play";
+export const PLAY_ICON_PLAY_SELECTOR = ".Play__icon-play";
+export const PLAY_ICON_STOP_SELECTOR = ".Play__icon-stop";
+
+// Shape
+export const SHAPE_ACTIVE_BEAT_COLOR_OTHER = "#d1d1d1";
+export const SHAPE_ACTIVE_BEAT_COLOR_CURRENT = "#151515";
+export const SHAPE_INACTIVE_BEAT_COLOR = "#EEEEEE";
+export const SHAPE_GRID_BORDER_COLOR = "#151515";
+export const SHAPE_SIZE_MOBILE = 25;
+export const SHAPE_SIZE_TABLET = 20;
+export const SHAPE_SIZE_DESKTOP = 32;
+export const SHAPE_DOT_START_ANGLE = 0;
+export const SHAPE_DOT_END_ANGLE = 2 * Math.PI;
+
+// SwitchBeats
+export const SWITCH_BEATS_CHIP_SELECTOR = ".SwitchBeats";
+
+// Tap
+export const TAP_CHIP_SELECTOR = ".Tap";
+
+///////////////////
+// Constants
+///////////////////
+
+// App
+export const APP_SOUND_DELAY = 0.01;
+export const URL: string = "http://192.168.0.56:3000/";
+
 // KeyCodes
 export const ESC_KEY_CODE = 27;
+
+// Shared constants
+export const SHOW_CLASS = "-show";
+export const BORDER_RADIUS = 6;
 
 // Time constants
 export const SIXTY_SECONDS = 60000;
 export const ONE_MINUTE_IN_SECONDS = 60.0;
 
-// Shared constants
-export const SHOW_CLASS = "-show";
-export const BORDER_RADIUS = 6;
+// Viewports
+export const MOBILE_VIEWPORT = 0;
+export const TABLET_VIEWPORT = 700;
+export const DESKTOP_VIEWPORT = 1000;
+
+// Workers
+export const WORKER_START_MESSAGE = "start";
+export const WORKER_STOP_MESSAGE = "stop";
