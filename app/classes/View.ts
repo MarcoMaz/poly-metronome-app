13b8971a681f47e91f7c79813daee70e48b80411
Marco Mazzai <mazzai.marco@gmail.com>
2023-05-14 21:57:30 +0200

    - fix view

--- app/classes/View.ts
+++ app/classes/View.ts
@@ -1,17 +1,15 @@
-import Engine from "./Engine";
-import Metronome from "./Metronome";
-import CanvasPicker from "./CanvasPicker";
-
-// Refactor styling
-import Modal from "./Modal";
-import SwitchBeatsChip from "./SwitchBeatsChip";
-import TapChip from "./TapChip";
-import PlayButton from "./PlayButton";
-import MuteButton from "./MuteButton";
 import AgainstBeatPicker from "./AgainstBeatPicker";
 import BaseBeatPicker from "./BaseBeatPicker";
 import BpmKnob from "./BpmKnob";
+import CanvasPicker from "./CanvasPicker";
 import ControllerPanel from "./ControllerPanel";
+import Engine from "./Engine";
+import Metronome from "./Metronome";
+import Modal from "./Modal";
+import MuteButton from "./MuteButton";
+import PlayButton from "./PlayButton";
+import SwitchBeatsChip from "./SwitchBeatsChip";
+import TapChip from "./TapChip";
 
 import { BEAT_MAX, BEAT_MIN } from "./base/constants";
 
@@ -20,32 +18,30 @@ import { BEAT_MAX, BEAT_MIN } from "./base/constants";
  *
  * @name View
  *
- * @param {CanvasPicker} canvasPicker           - The UI controlling the buttons selection of the different type of metronome representation.
- * @param {BpmUi} bpm                           - The UI controlling the bpm of the metronome.
- * @param {BpmKnob} BpmKnob                     - The UI controlling the bpm of the metronome.
- * @param {controllersContainerUi} controllers  - The UI controlling the app functionality.
- * @param {Modal} modal                         - The UI controlling the modal appearing when the user selects a combination which is not a polyrhythm.
- * @param {SwitchBeatsChip} switchBeatsChip     - The UI controlling the switchBeats chip.
- * @param {TapChip} tapChip                     - The UI controlling the tap chip.
- * @param {PlayButton} playButton               - The UI controlling the play chip.
- * @param {MuteButton} muteButton               - The UI controlling the mute chip.
  * @param {AgainstBeatPicker} againstBeatPicker - The UI controlling the againstBeatPicker.
  * @param {BaseBeatPicker} baseBeatPicker       - The UI controlling the baseBeatPicker.
+ * @param {BpmKnob} BpmKnob                     - The UI controlling the bpm of the metronome.
+ * @param {CanvasPicker} canvasPicker           - The UI controlling the buttons selection of the different type of metronome representation.
  * @param {ControllerPanel} controllerPanel     - The UI controlling the controllerPanels.
+ * @param {Modal} modal                         - The UI controlling the modal appearing when the user selects a combination which is not a polyrhythm.
+ * @param {MuteButton} muteButton               - The UI controlling the mute chip.
+ * @param {PlayButton} playButton               - The UI controlling the play chip.
+ * @param {SwitchBeatsChip} switchBeatsChip     - The UI controlling the switchBeats chip.
+ * @param {TapChip} tapChip                     - The UI controlling the tap chip.
  *
  */
 
 class View {
-  public canvasPicker: CanvasPicker;
-  private modal: Modal;
-  private switchBeatsChip: SwitchBeatsChip;
-  private tapChip: TapChip;
-  private playButton: PlayButton;
-  private muteButton: MuteButton;
   private againstBeatPicker: AgainstBeatPicker;
   private baseBeatPicker: AgainstBeatPicker;
   private bpmKnob: BpmKnob;
+  public canvasPicker: CanvasPicker;
   private controllerPanel: ControllerPanel;
+  private modal: Modal;
+  private muteButton: MuteButton;
+  private playButton: PlayButton;
+  private switchBeatsChip: SwitchBeatsChip;
+  private tapChip: TapChip;
 
   /**
    * Define DOM Elements
